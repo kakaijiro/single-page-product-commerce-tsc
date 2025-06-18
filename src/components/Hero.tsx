3c45521ce0b234785d3a0f7bebd940916d988585
kakaijiro <kakaijiro@gmail.com>
2025-06-18 15:48:57 +0900

    feat: add Hero and HeroCarousel components with carousel functionality and integrate into ProductsGrid

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+import HeroCarousel from "./HeroCarousel";
+
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className="gird-cols-1 grid items-center gap-24 lg:grid-cols-2">
+      <div>
+        <h1 className="max-w-2xl text-4xl font-bold tracking-tight sm:text-6xl">
+          We are changing the way people shop
+        </h1>
+        <p className="mt-8 max-w-xl text-lg leading-8">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus
+          nihil, totam deleniti dolor labore vel magnam libero iusto quidem ex
+          iure, amet facere impedit doloremque qui aut odio! Quam, quis.
+        </p>
+        <Button asChild size="lg" className="mt-10">
+          <Link to="/products">Our Products</Link>
+        </Button>
+      </div>
+      <HeroCarousel />
+    </section>
+  );
 }
 
 export default Hero;
--- src/components/ProductsGrid.tsx
+++ src/components/ProductsGrid.tsx
@@ -1,7 +1,34 @@
+import { Link, useLoaderData } from "react-router-dom";
+import { Card, CardContent } from "./ui/card";
+import { formatAsDollars, type ProductsResponse } from "@/utils";
+
 function ProductsGrid() {
+  const { data: products } = useLoaderData() as ProductsResponse;
   return (
     // product gird
-    <div>Product Grid</div>
+    <div className="grid gap-4 pt-12 md:grid-cols-2 lg:grid-cols-3">
+      {products.map((product) => {
+        const { title, price, image } = product.attributes;
+        const dollarsAmout = formatAsDollars(price);
+        return (
+          <Link to={`/products/${product.id}`} key={product.id}>
+            <Card>
+              <CardContent className="p-4">
+                <img
+                  src={image}
+                  alt={title}
+                  className="tranform h-64 w-full rounded-md object-cover transition-transform duration-300 ease-out hover:scale-105 md:h-48"
+                />
+                <div className="mt-4 text-center">
+                  <h2 className="text-xl font-semibold capitalize">{title}</h2>
+                  <p className="text-primary mt-2 font-light">{dollarsAmout}</p>
+                </div>
+              </CardContent>
+            </Card>
+          </Link>
+        );
+      })}
+    </div>
   );
 }
 
