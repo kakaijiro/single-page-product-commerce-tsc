922ec8fb047700a06b67b90375af3a8932a874a7
kakaijiro <kakaijiro@gmail.com>
2025-06-22 08:00:19 +0900

    feat: implement pagination components and utility functions; add PaginationContainer and pagination utilities for navigation

--- src/components/PaginationContainer.tsx
+++ src/components/PaginationContainer.tsx
@@ -1,5 +1,62 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "./ui/pagination";
+import {
+  type ProductsResponseWithParams,
+  constructUrl,
+  constructPrevOrNextUrl,
+} from "@/utils";
+
+import { useLoaderData, useLocation } from "react-router-dom";
+
 function PaginationContainer() {
-  return <div>Pagination Container</div>;
+  const { meta } = useLoaderData() as ProductsResponseWithParams;
+  const { pageCount, page } = meta.pagination;
+  const { search, pathname } = useLocation();
+
+  const pages = Array.from({ length: pageCount }, (_, index) => index + 1);
+
+  // if pages is 1 or less, we assume no need to display pagination
+  if (pageCount < 2) return null;
+
+  const renderPagination = pages.map((pageNumber) => {
+    const isActive = pageNumber === page;
+    const url = constructUrl({ pageNumber, search, pathname });
+
+    return (
+      <PaginationItem key={pageNumber}>
+        <PaginationLink to={url} isActive={isActive}>
+          {pageNumber}
+        </PaginationLink>
+      </PaginationItem>
+    );
+  });
+
+  const { prevUrl, nextUrl } = constructPrevOrNextUrl({
+    currentPage: page,
+    pageCount,
+    search,
+    pathname,
+  });
+
+  return (
+    <Pagination className="mt-16">
+      <PaginationContent>
+        <PaginationItem key="prev">
+          <PaginationPrevious to={prevUrl} />
+        </PaginationItem>
+        {renderPagination}
+        <PaginationItem key="next">
+          <PaginationNext to={nextUrl} />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
 }
 
 export default PaginationContainer;
